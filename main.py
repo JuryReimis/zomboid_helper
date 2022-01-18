de1a563ccb59f6e7d9dd3db7e9c21ac921b0e7c3
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2022-01-18 19:09:04 +0300

    Добавлена функция record_info_from_file, позваляет считывать информацию из инфо-файлов.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import time
-from collections import namedtuple
 import json
 
 os.chdir("D:\\Steam\\steamapps\\workshop\\content\\108600")
@@ -8,13 +7,12 @@ os.chdir("D:\\Steam\\steamapps\\workshop\\content\\108600")
 
 def check_modify(dir_name: str):
     print(dir_name)
-    last_check_time = namedtuple("years", "tm_year, tm_mon, tm_mday")
-    latest_update_time = last_check_time(tm_year=2022, tm_mon=1, tm_mday=14)
+    zomboid_place = find_dir(name="Zomboid", start_drive="C:\\Users")
+    latest_update_time = record_info_from_file(flag="y")
     updated_files = {}
     time_of_update = {}
     walk = os.walk(dir_name)
     for path, directories, files in walk:
-        print(path, directories)
         for file in files:
             now_check = "\\".join([path, file])
             file_lm = time.gmtime(os.path.getmtime(now_check))
@@ -22,12 +20,29 @@ def check_modify(dir_name: str):
                 latest_update_time = file_lm
                 relative_path = now_check.replace(os.getcwd() + "\\", "").split(sep="\\")
                 time_of_update[relative_path[2]] = file_lm
-                updated_files[(relative_path[2])] = f"{relative_path[0]} - {time.strftime('%Y-%m-%d %H:%M', time_of_update[relative_path[2]])}"
-    file_name = f"{time.strftime('%Y-%m-%d', time.gmtime(time.time()))}.txt"
-    zomboid_place = find_dir(name="Zomboid", start_drive="C:\\Users")
+                updated_files[(relative_path[
+                    2])] = f"{relative_path[0]} - {time.strftime('%Y-%m-%d %H:%M', time_of_update[relative_path[2]])}"
+    updated_files["Отчет сделан"] = time.strftime('%Y-%m-%d %H:%M', time.gmtime(time.time()))
+    file_name = f"{time.strftime(r'%Y-%m-%d', time.gmtime(time.time()))}.txt"
     check_existence_dir(zomboid_place, "mod updates")
     with open(file=os.path.join(zomboid_place, "mod updates", file_name), mode="w", encoding="utf-8") as f:
-        print(json.dumps(updated_files, indent=4), f"\nПоследнее найденное обновление файлов: {time.strftime('%Y-%m-%d %H:%M', latest_update_time)}", file=f)
+        print(json.dumps(updated_files, indent=4, ensure_ascii=False), file=f)
+
+
+def record_info_from_file(flag: str, path: str = r"C:\Users\indli\Zomboid\mod updates"):
+    if flag.lower() == "y":
+        file_name = "default"
+        latest_record = time.gmtime(os.path.getmtime(path))
+        for file in os.scandir(path):
+            file: os.DirEntry
+            file_m_time = time.gmtime(os.path.getmtime(file.path))
+            if file_m_time >= latest_record:
+                latest_record = file_m_time
+                file_name = file.path
+        with open(file=file_name, mode="r", encoding="utf-8") as f:
+            return time.strptime(json.load(f)["Отчет сделан"], "%Y-%m-%d %H:%M")
+    else:
+        return time.gmtime(time.time())
 
 
 def find_dir(name: str, start_drive: str) -> str:
@@ -44,5 +59,4 @@ def check_existence_dir(path, name_dir):
         return print(f"Директория {name_dir} создана")
 
 
-# os.path.relpath(now_check, os.getcwd())
 check_modify(os.getcwd())
