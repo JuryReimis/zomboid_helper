a2995ccef1773ec7b3b301d31090417629c0c57e
JuryReimis <80026660+JuryReimis@users.noreply.github.com>
2022-01-22 15:46:25 +0300

    В функции check_modify добавлен возврат словаря с данными из последнего созданного файла и директории с игрой Добавлена функция, которая копирует все файлы, еще не дописана. Добавлена функция get_realpath_of_mods которая позволяет из путя 21356498/mods/mod_name получить имя мода В функцию record_info_from_file добавлен еще один флаг "last", который дает понять, нужно получить запись из последнего созданного файла, независимо от даты его создания, или из последнего файлаЮ который был создан в другой день. Теперь эта функция возвращает не дату, а словарь с названиями и датами обновления модов. Добавлен консольный интерфейс, который позваляет узнать у пользователя хочет ли он автоматически скопировать все обновленные файлы

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import nt
 import os
+import shutil
 import sys
 import time
 import json
@@ -6,7 +8,8 @@ import json
 
 def check_modify(dir_name: str):
     zomboid_dir = find_dir(name="Zomboid", start_path="C:\\Users")
-    latest_update_time = record_info_from_file(flag=check_existence_dir(zomboid_dir, "mod updates"))
+    info_from_file = record_info_from_file(flag=check_existence_dir(zomboid_dir, "mod updates"))
+    latest_update_time = time.strptime(info_from_file["Отчет сделан"], "%Y-%m-%d %H:%M")
     updated_files = {}
     time_of_update = {}
     walk = os.walk(dir_name)
@@ -26,10 +29,34 @@ def check_modify(dir_name: str):
         print(json.dumps(dict(sorted(updated_files.items(), key=lambda x: x[0].lower())), indent=4, ensure_ascii=False),
               file=f)
         print(f"По пути {os.path.join(zomboid_dir, 'mod updates')} находятся документы с информацией о всех изменениях")
+    info_from_file = record_info_from_file(flag=check_existence_dir(zomboid_dir, "mod updates"), last=True)
+    return {"zomboid_dir": zomboid_dir, "info_from_file": info_from_file}
 
 
-def record_info_from_file(flag: bool, path: str = r"C:\Users\indli\Zomboid\mod updates"):
-    if flag is True:
+def copy_modify(zomboid_dir: str, workshop_dir: str, info_from_file: dict) -> None:
+    print("копирование")
+    if len(info_from_file.keys()) < 2:
+        for mod in get_realpath_of_mods(workshop_dir=workshop_dir):
+            print(f"Копирую {os.path.basename(mod)}")
+            shutil.copytree(mod, os.path.join(zomboid_dir, "mods", os.path.basename(mod)), dirs_exist_ok=True)
+
+
+def get_realpath_of_mods(workshop_dir: str, mods_filter=None):
+    list_of_mods = []
+    for item in os.scandir(workshop_dir):
+        item: nt.DirEntry
+        for name in os.scandir(item.path):
+            name: nt.DirEntry
+            if name.name == "mods":
+                for mod in os.scandir(name.path):
+                    mod: nt.DirEntry
+                    if mods_filter is None or mod.name in mods_filter:
+                        list_of_mods.append(mod.path)
+    return list_of_mods
+
+
+def record_info_from_file(flag: bool, path: str = r"C:\Users\indli\Zomboid\mod updates", last=False) -> dict:
+    if flag is True and not next(os.scandir(path), None) is None:
         file_name = "default"
         dir_creation_time = time.gmtime(os.path.getctime(path))
         for file in os.scandir(path):
@@ -37,12 +64,15 @@ def record_info_from_file(flag: bool, path: str = r"C:\Users\indli\Zomboid\mod u
             file_m_time = time.gmtime(os.path.getmtime(file.path))
             if file_m_time >= dir_creation_time:
                 dir_creation_time = file_m_time
-                if file.name != time.strftime("%Y-%m-%d", time.gmtime(time.time()))+".txt":
+                if file.name != time.strftime("%Y-%m-%d", time.gmtime(time.time())) + ".txt":
+                    file_name = file.path
+                elif last is True:
                     file_name = file.path
         with open(file=file_name, mode="r", encoding="utf-8") as f:
-            return time.strptime(json.load(f)["Отчет сделан"], "%Y-%m-%d %H:%M")
+            return json.load(f)
     else:
-        return time.gmtime(time.time())
+        print("Документы с информацией об обновлении не найдены")
+        return {"Отчет сделан": time.gmtime(time.time())}
 
 
 def find_dir(start_path: str, name: str = "content\\108600") -> str:
@@ -52,7 +82,8 @@ def find_dir(start_path: str, name: str = "content\\108600") -> str:
             d = os.path.join(path, name)
             print(f"Найдена директория {d}")
             return d
-    print("По указанным данным ничего не найдено")
+    print("По указанным данным ничего не найдено\nЗавершаю программу")
+    sys.exit()
 
 
 def check_existence_dir(path, name_dir):
@@ -83,13 +114,21 @@ def main():
     if user_call_start_searching.lower() == "y":
         print("Приступаю к поиску...")
         workshop_dir = find_dir(start_path=user_call_workshop_path)
-        check_modify(workshop_dir)
+        check_modify_dict = check_modify(workshop_dir)
     elif user_call_start_searching.lower() == "n":
         print("Завершаю программу")
         sys.exit()
     else:
         print("Ответ не читаем, повторите попытку")
         sys.exit()
+    call_user_copy_files = input(
+        f"""Приступить к копированию обновленных файлов в корневую директорию 
+        {os.path.realpath(check_modify_dict['zomboid_dir'])} 
+        в автоматическом режиме? y/n? """)
+    if call_user_copy_files.lower() == "y":
+        copy_modify(zomboid_dir=check_modify_dict["zomboid_dir"], workshop_dir=workshop_dir,
+                    info_from_file=check_modify_dict["info_from_file"])
+    print("Программа завершена!")
 
 
 if __name__ == "__main__":
